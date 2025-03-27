a48a9320d05e3b38a1cb9c7ed0c336493b6771ad
RapidTypist <rapidtypistofficial@gmail.com>
2025-03-27 22:11:46 +0530

    Add files via upload

---
+++ terms.js
@@ -0,0 +1,8 @@
+
+// Function to toggle between Privacy Policy and Terms & Conditions
+function showSection(sectionId) {
+    document.querySelectorAll('.content-section').forEach(section => {
+        section.classList.add('hidden');
+    });
+    document.getElementById(sectionId).classList.remove('hidden');
+}
